d96e015135004275998c4c42c6c6d471c35667a6
Jordan Bacon <jmbacon92@gmail.com>
2019-06-22 09:00:38 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -18,11 +18,7 @@ contacts["Freddy Mercury"].each do |key, value|
 if key == :favorite_icecream_flavors
   value.delete_if {|value| value == "strawberry"}
 end
-  contacts["Freddy Mercury"].each do |key, value|
-    if key == :favorite_icecream_flavors
-      value.delete_if {|x| x == "strawberry"}
-    end
-  end
+end
   #remember to return your newly altered contacts hash!
   contacts
 end
